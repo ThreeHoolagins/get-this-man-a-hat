7a52a6dcc3f701d5250548971767f23f9af78549
Caleb C <cbcunningham20@gmail.com>
2024-05-04 17:27:20 -0500

    updates to clean up some of ui

--- js/app.js
+++ js/app.js
@@ -1,8 +1,38 @@
 var canvas;
 
+const downloadImage = () => {
+  const dataURL = canvas.toDataURL({
+    width: canvas.width,
+    height: canvas.height,
+    left: 0,
+    top: 0,
+    format: 'png',
+  });
+  const link = document.createElement('a');
+  link.download = 'image.png';
+  link.href = dataURL;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+const flipImage = () => {
+  canvas.getObjects()[0].set('flipX', !canvas.getObjects()[0].flipX);
+  canvas.renderAll();
+}
+
+const centerCowboyHat = () => {
+  canvas.getObjects().map(function(o) {
+    o.setLeft(0);
+    o.setTop(0);
+    
+    canvas.renderAll();
+  });
+}
+
 const initializeCanvas = () => {
     canvas = new fabric.Canvas('cowboyHatInator',  {
-      backgroundColor: 'rgb(100,100,200)',
+      backgroundColor: 'rgba(0,0,0,0)',
       width: 1200, height: 600
     });
 
@@ -14,42 +44,23 @@ const initializeCanvas = () => {
       left: 300
     });
     canvas.add(imgInstance);
-}
 
-const downloadImage = () => {
-   const dataURL = canvas.toDataURL({
-       width: canvas.width,
-       height: canvas.height,
-       left: 0,
-       top: 0,
-       format: 'png',
-   });
-   const link = document.createElement('a');
-   link.download = 'image.png';
-   link.href = dataURL;
-   document.body.appendChild(link);
-   link.click();
-   document.body.removeChild(link);
-}
-
-const uploadImageToCanvasForHat = () => {
-  const selectedFile = document.getElementById("uploadedImage").files[0];
-  if (selectedFile == undefined) {
-    window.alert("ur mom homo but not in a bad way just a homo way")
-  }
-  else {
-    var reader = new FileReader();
-    reader.onload = function(f) {
-       var data = f.target.result;
-       fabric.Image.fromURL(data, function(img) {
-          canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
-             scaleX: canvas.width / img.width,
-             scaleY: canvas.height / img.height
-          });
-       });
-    };
-    reader.readAsDataURL(selectedFile);
-  }
+    document.getElementById('uploadedImage').addEventListener("change", (ev) => {
+      const selectedFile = document.getElementById("uploadedImage").files[0];
+      if (selectedFile !== undefined) {
+        var reader = new FileReader();
+        reader.onload = function(f) {
+           var data = f.target.result;
+           fabric.Image.fromURL(data, function(img) {
+              canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas));
+              canvas.setHeight(img.height);
+              canvas.setWidth(img.width);
+           });
+        }
+        reader.readAsDataURL(selectedFile);
+        centerCowboyHat()
+      }
+    });
 }
 
 document.addEventListener("DOMContentLoaded", initializeCanvas)
